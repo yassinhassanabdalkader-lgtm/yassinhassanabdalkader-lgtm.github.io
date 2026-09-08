42f80ac52266d46e23a442ba59f052b98b5b6d67
yassinhassanabdalkader-lgtm <yassinhassanabdalkader@gmail.com>
2026-09-08 15:32:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const items=document.querySelectorAll('.reveal');const io=new IntersectionObserver((entries)=>{entries.forEach(e=>{if(e.isIntersecting){e.target.classList.add('show');io.unobserve(e.target)}})},{threshold:.12});items.forEach(i=>io.observe(i));
